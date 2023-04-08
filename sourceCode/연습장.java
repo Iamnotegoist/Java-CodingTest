634d410d9f0005bda3e951a7f4a2398dfc73430c
Iamnotegoist <kiju9512@gmail.com>
2023-04-08 18:14:12 +0900

    디렉토리 추가

--- sourceCode/연습장.java
+++ sourceCode/연습장.java
@@ -1,4 +1,4 @@
-package java_codingTest;
+package java_codingTest.sourceCode;
 
 import java.util.ArrayList;
 import java.util.List;
--- sourceCode/완전탐색_문제18.java
+++ sourceCode/완전탐색_문제18.java
@@ -1,4 +1,4 @@
-package java_codingTest;
+package java_codingTest.sourceCode;
 
 import java.util.stream.IntStream;
 
